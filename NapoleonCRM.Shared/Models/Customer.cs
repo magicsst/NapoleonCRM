8038e98c062c06b85f8d47708ed470a05941bf30
Pantazis Siklarlis <magicsst@hotmail.com>
2025-07-12 17:37:06 +0300

    Add project files.

---
+++ NapoleonCRM.Shared/Models/Customer.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace NapoleonCRM.Shared.Models;
+
+[DataContract]
+public class Customer
+{
+    [Key]
+    [DataMember]
+    public long? Id { get; set; }
+
+    [DataMember]
+    public string? Name { get; set; }
+
+    [DataMember]
+    public string? Type { get; set; }
+
+    [DataMember]
+    public string? Industry { get; set; }
+
+    [DataMember]
+    public long? AddressId { get; set; }
+
+    [DataMember]
+    public long? ContactId { get; set; }
+
+    [DataMember]
+    public string? Logo { get; set; }
+
+    [DataMember]
+    public string? Notes { get; set; }
+
+    [DataMember]
+    public Address? Address { get; set; }
+
+    [DataMember]
+    public List<Contact>? Contact { get; set; }
+
+     [DataMember]
+    public DateTime? CreatedDate { get; set; }
+
+    [DataMember]
+    public DateTime ModifiedDate { get; set; }
+
+}
